7075e54db0c20ca1d417c4f2177e5c5e99fd15e4
Yurij Sopun <yurakarpovichminsk@gmail.com>
2024-01-04 21:37:10 +0200

    add slider autoplay

--- js/menu.js
+++ js/menu.js
@@ -27,15 +27,8 @@ const swiper = new Swiper('.swiper', {
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
+    autoplay: {
+        delay: 3000,
+      }
   });
